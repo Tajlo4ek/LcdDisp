5ef1c65225c0918ab74d2277ea71536fee95b14d
Алексей Беляев <aleksey.belyaev.asd@gmail.com>
2021-10-08 16:27:57 +0300

    Create visualizer control

---
+++ src/Controls/VisualizerControl.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "BaseControl.h"
+
+namespace Controls
+{
+    class VisualizerControl : public BaseControl
+    {
+    private:
+        uint16_t lowColor;
+        uint16_t mediumColor;
+        uint16_t highColor;
+        uint16_t maxColor;
+
+        int spectrumLineCount;
+        byte *nowLeftSpectrum;
+        byte *maxLeftSpectrumData;
+
+        byte *nowRightSpectrum;
+        byte *maxRightSpectrumData;
+
+        int spectrumMaxSize;
+        int spectrumMaxSizeDiv3;
+
+    public:
+        VisualizerControl(TFT_eSPI *lcd, ControlRect rect);
+        ~VisualizerControl();
+
+        void DrawSpectrum(byte *spectrumLeft, byte *spectrumRight);
+        const int GetLineCount() const;
+        const int GetMaxLineLength() const;
+
+        void Reset();
+        void ReDraw() override;
+    };
+
+}
